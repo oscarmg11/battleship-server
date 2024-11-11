05ef833666788da61763ab6ce85be6c5f62edc37
Oscar Moreno <oscar@pidedirecto.mx>
2024-11-10 23:13:36 -0600

    add sendWebSocketEvent

---
+++ src/webSocketEvent/sendWebSocketEvent.ts
@@ -0,0 +1,28 @@
+import { ApiGatewayManagementApi } from '@aws-sdk/client-apigatewaymanagementapi'
+
+export async function sendWebSocketEvent({
+    connectionId,
+    event,
+    data,
+}: Params): Promise<void> {
+    const apiGatewayManagementApi = new ApiGatewayManagementApi({
+        endpoint:
+            process.env.ENVIRONMENT === 'DEV' ? 'http://localhost:3001' : '',
+    })
+
+    await apiGatewayManagementApi.postToConnection({
+        ConnectionId: connectionId,
+        Data: JSON.stringify({
+            event,
+            data,
+        }),
+    })
+}
+
+type Params = {
+    connectionId: string
+    event: Event
+    data: any
+}
+
+type Event = 'onGameCreated' | 'onError'
