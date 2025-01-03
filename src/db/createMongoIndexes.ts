aa422b92e619d378514e23773d20c57c2ba5a15f
Oscar Moreno <oscar@pidedirecto.mx>
2025-01-03 17:40:30 -0600

    add createMongoIndexes

---
+++ src/db/createMongoIndexes.ts
@@ -0,0 +1,17 @@
+import { createDbConnection } from '@/db/createDbConnection'
+
+export async function createMongoIndexes() {
+    const connection = await createDbConnection()
+
+    await connection
+        .db('battleship')
+        .collection('game')
+        .createIndex(
+            { deleteAt: -1 },
+            { background: true, expireAfterSeconds: 0 },
+        )
+    await connection
+        .db('battleship')
+        .collection('game')
+        .createIndex({ roomId: -1 }, { background: true })
+}
